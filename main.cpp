01d3682d3f8663561ce9355f83f0567c7fa545b2
cláudio junior <82659495+claudiojuniors@users.noreply.github.com>
2021-07-03 21:11:01 +0000

    Retirei o código anterior que estava comentado, vou tentar resolver os problemas nesse novo código

--- main.cpp
+++ main.cpp
@@ -82,25 +82,26 @@ int main (){
 
 	cout << "Digite 1 se quiser gravar o arquivo, digite 2 para ler o arquivo"
 		 << endl;
-	cin >> opcao << endl;
+	cin >> opcao;
 
 	if (opcao == 1) {
 		Gravar.open("Complexos.txt", ios::out);
 
 		while (opcao == 1) {
 			cout << "Digite a parte REAL do número complexo" << endl;
-			cin >> Real << endl;
+			cin >> Real;
 
 			cout << "Digite a parte IMAGINARIA do numero complexo" << endl;
-			cin >>imag << endl;
+			cin >>imag;
 
 			incluirNoFim(Real, imag);
 
 			cout << "Deseja continuar digitando? (se SIM aperte 1, se "
 					"NÃO,aperte 2)"
 				 << endl;
-			cin >> opcao << endl;
+			cin >> opcao;
 		}
+		Gravar<<Real<<"+"<<imag<<"i"<<endl;
 	}
 	Gravar.close();
 
@@ -111,118 +112,3 @@ int main (){
 	Ler.close();
 }
 
-
-
-/*#include <fstream>
-#include <iostream>
-using namespace std;
-
-/*struct tComplexos{
-	  float *real;
-	  float *imag;
-	};
-
-struct tComplexos {
-	int info;
-	tComplexos *proximo;
-};
-
-tComplexos *criaComplex(int item) {
-	tComplexos *Complex = new tComplexos;
-
-	Complex->info = item;
-	Complex->proximo = NULL;
-
-	return Complex;
-}
-
-struct tLista {
-  tComplexos *primeiro;
-	tComplexos *ultimo;
-	tComplexos *marcador;
-	int tamanho;
-};
-
-void inicializaLista(tLista *pLista) {
-	pLista->primeiro = NULL;
-	pLista->ultimo = NULL;
-	pLista->marcador = NULL;
-	pLista->tamanho = 0;
-}
-
-bool obterTamanho(tLista *pLista) {
-	return pLista->tamanho;
-}
-bool listaVazia(tLista *pLista) {
-	return (pLista->tamanho == 0);
-}
-
-bool finalLista(tLista *pLista) {
-	return (pLista->marcador == NULL);
-}
-
-void incluirNoFim(tLista *pLista, int info) {
-	tComplexos *Complex;
-	Complex = criaComplex(info);
-
-	if (listaVazia(pLista)) {
-		pLista->primeiro = Complex;
-	} else {
-		pLista->ultimo->proximo = Complex;
-	}
-
-	pLista->ultimo = Complex;
-	pLista->marcador = Complex;
-	pLista->tamanho++;
-}
-
-void imprimirLista(tLista* pLista){
-  pLista->marcador = pLista->primeiro;
-
-  while(!finalLista(pLista)){
-    int informacao = pLista->marcador->info;
-    cout << "A informacao eh: " << informacao << endl;
-    
-    pLista->marcador = pLista->marcador->proximo;
-  }
-}
-int main() {
-	int opt, Real, imag;
-
-	ofstream Gravar;
-	ifstream Ler;
-
-	tLista *Complexos = new tLista;
-
-	inicializaLista(Complexos);
-
-	cout << "Digite 1 se quiser gravar o arquivo, digite 2 para ler o arquivo"
-		 << endl;
-	cin >> opt << endl;
-
-	if (opt == 1) {
-		Gravar.open("Complexos.txt", ios::out);
-
-		while (opt == 1) {
-			cout << "Digite a parte REAL do número complexo" << endl;
-			cin >> Real << endl;
-
-			cout << "Digite a parte IMAGINARIA do numero complexo" << endl;
-			cin >> imag << endl;
-
-			incluirNoFim(Real, imag);
-
-			cout << "Deseja continuar digitando? (se SIM aperte 1, se "
-					"NÃO,aperte 2)"
-				 << endl;
-			cin >> opt << endl;
-		}
-	}
-	Gravar.close();
-
-	if (opt==2){
-		Ler.open("Complexos.txt", ios::in);
-	 imprimirLista();
-	}
-	Ler.close();
-}*/
